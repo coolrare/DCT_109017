640b50517bb57464cd84790e37dfb2caea7f6750
黃升煌 Mike <mike@miniasp.com>
2020-07-12 16:42:45 +0800

    @Input / @Output

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,19 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Output,
+  EventEmitter,
+  Input,
+  SimpleChanges,
+  OnChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnChanges {
   counter = 0;
-  keyword = '';
   isHighlight = false;
   fontSize = 30;
+
+  // _keyword = '';
+  // @Input() set keyword(value) {
+  //   this._keyword = value;
+  // }
+
+  // get keyword(){
+  //   return this._keyword;
+  // }
+
+  @Input() keyword = '';
+  @Output() keywordChange = new EventEmitter();
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  ngOnChanges(simpleChanges: SimpleChanges) {
+    console.log(simpleChanges);
+  }
 
   buttonClick(event: MouseEvent) {
     console.log('test');
@@ -24,13 +47,13 @@ export class HeaderComponent implements OnInit {
     console.log(event);
     this.isHighlight = !this.isHighlight;
     this.fontSize += 2;
+
+    this.keywordChange.emit(this.keyword);
   }
 
-  keywordInput(event: InputEvent){
+  keywordInput(event: InputEvent) {
     // console.log(event);
     this.counter = +(event.target as HTMLInputElement).value;
     // console.log(event.target );
-
   }
-
 }
